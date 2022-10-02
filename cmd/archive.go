2c38689f6c5a81f2c90066a0c44fb180c6a4a435
Yannick Schutz <hello@yannickschutz.com>
2022-10-02 22:31:17 +0200

    wbep montage

--- cmd/archive.go
+++ cmd/archive.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"path"
 	"path/filepath"
+	"sync"
 
 	"github.com/spf13/cobra"
 	"github.com/ys/rolls/roll"
@@ -23,6 +24,8 @@ var archiveCmd = &cobra.Command{
 	Short: "Rename folder and files and add exif",
 	Args:  cobra.MatchAll(cobra.MaximumNArgs(1), cobra.OnlyValidArgs),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		imagick.Initialize()
+		defer imagick.Terminate()
 		year, err := cmd.PersistentFlags().GetInt("year")
 		cobra.CheckErr(err)
 		var rollNumber string
@@ -49,63 +52,73 @@ var archiveCmd = &cobra.Command{
 }
 
 func archive(rolls []roll.Roll) {
-	for _, roll := range rolls {
-		fmt.Println(roll.Metadata.RollNumber)
-		// Rename Files
-		files, err := ioutil.ReadDir(roll.Folder)
-		cobra.CheckErr(err)
+	var wg sync.WaitGroup
+	wg.Add(len(rolls))
 
-		imagick.Initialize()
-		defer imagick.Terminate()
-		dw := imagick.NewDrawingWand()
-		defer dw.Destroy()
-		mw := imagick.NewMagickWand()
-		defer mw.Destroy()
-		wand := imagick.NewPixelWand()
-		wand.SetColor("transparent")
-		defer wand.Destroy()
-		mw.SetBackgroundColor(wand)
-		for i, file := range files {
-			if filepath.Ext(file.Name()) == ".md" {
-				continue
+	for _, r := range rolls {
+		go func(roll roll.Roll) {
+			fmt.Println(roll.Metadata.RollNumber)
+			// Rename Files
+			files, err := ioutil.ReadDir(roll.Folder)
+			cobra.CheckErr(err)
+
+			dw := imagick.NewDrawingWand()
+			defer dw.Destroy()
+			mw := imagick.NewMagickWand()
+			defer mw.Destroy()
+			wand := imagick.NewPixelWand()
+			wand.SetColor("transparent")
+			defer wand.Destroy()
+			mw.SetBackgroundColor(wand)
+			// assume all files are images but the md file
+			imagesCount := len(files) - 1
+			var imagesWidth, imagesHeight int
+			switch {
+			case imagesCount < 12:
+				imagesWidth, imagesHeight = 4, 3
+			case imagesCount < 30:
+				imagesWidth, imagesHeight = 5, 6
+			case imagesCount < 36:
+				imagesWidth, imagesHeight = 6, 6
+			case imagesCount > 36:
+				imagesWidth, imagesHeight = 6, 7
 			}
-			newName := fmt.Sprintf("%s-%s-%02d%s", roll.Metadata.RollNumber, roll.Metadata.ShotAt.Format("0102"), i+1, filepath.Ext(file.Name()))
+			for i, file := range files {
+				if filepath.Ext(file.Name()) == ".md" {
+					continue
+				}
+				newName := fmt.Sprintf("%s-%s-%02d%s", roll.Metadata.RollNumber, roll.Metadata.ShotAt.Format("0102"), i+1, filepath.Ext(file.Name()))
 
-			fmt.Println("Rename File ", file.Name(), " -> ", newName)
-			tmw := imagick.NewMagickWand()
-			defer tmw.Destroy()
-			tmw.ReadImage(path.Join(roll.Folder, file.Name()))
-			tmw = tmw.Clone()
-			tmw.AutoOrientImage()
-			tmw.SetBackgroundColor(wand)
-			width := tmw.GetImageWidth() / 20
-			height := tmw.GetImageHeight() / 20
-			tmw.ScaleImage(width, height)
-			mw.AddImage(tmw)
-		}
-		montage := mw.MontageImage(dw, "6x7+0+0", "200x200+2+2", imagick.MONTAGE_MODE_CONCATENATE, "0x0+0+0")
-		montage.SetBackgroundColor(wand)
-		current, err := os.Getwd()
-		if err != nil {
-			log.Println(err)
-		}
-		err = montage.WriteImage(path.Join(current, fmt.Sprintf("%s.png", roll.Metadata.RollNumber)))
-		// EXIF
-		// Create sheet
-		// montage
-		//  montage = MiniMagick::Tool::Montage.new
-		// montage.fill("gray")
-		// montage.background("#fff")
-		// montage.define("jpeg:size=200x200")
-		// montage.geometry("200x200+2+2")
-		// montage.auto_orient
-		// montage << "#{roll.dir}/*.jpg"
-		// montage << "#{destination}/#{roll.roll_number}.jpg"
-		// montage.call
-		// Rename Folder
-		newFolder := fmt.Sprintf("%s-%s-%s-%s", roll.Metadata.RollNumber, roll.Metadata.ShotAt.Format("0102"), roll.Metadata.CameraID, roll.Metadata.FilmID)
-		fmt.Println("Rename Folder ", roll.Folder, " -> ", newFolder)
+				fmt.Println("Rename File ", file.Name(), " -> ", newName)
+				ttmw := imagick.NewMagickWand()
+				ttmw.ReadImage(path.Join(roll.Folder, file.Name()))
+				tmw := ttmw.Clone()
+				ttmw.Destroy()
+
+				tmw.AutoOrientImage()
+				tmw.SetBackgroundColor(wand)
+				width := tmw.GetImageWidth() / 20
+				height := tmw.GetImageHeight() / 20
+				tmw.ScaleImage(width, height)
+				mw.AddImage(tmw)
+				tmw.Destroy()
+			}
+			// Create sheet
+			montage := mw.MontageImage(dw, fmt.Sprintf("%dx%d+0+0", imagesWidth, imagesHeight), "200x200+2+2", imagick.MONTAGE_MODE_CONCATENATE, "0x0+0+0")
+			montage.SetBackgroundColor(wand)
+			current, err := os.Getwd()
+			if err != nil {
+				log.Println(err)
+			}
+			err = montage.WriteImage(path.Join(current, fmt.Sprintf("%s.webp", roll.Metadata.RollNumber)))
+			// EXIF
+			// Rename Folder
+			newFolder := fmt.Sprintf("%s-%s-%s-%s", roll.Metadata.RollNumber, roll.Metadata.ShotAt.Format("0102"), roll.Metadata.CameraID, roll.Metadata.FilmID)
+			fmt.Println("Rename Folder ", roll.Folder, " -> ", newFolder)
+			wg.Done()
+		}(r)
 	}
+	wg.Wait()
 }
 
 func init() {
