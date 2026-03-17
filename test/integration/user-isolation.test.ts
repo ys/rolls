19ea8716740a645b9258cd51d1054096847a0189
Yannick Schutz <hello@yannickschutz.com>
2026-03-17 21:20:33 +0000

    gic

--- test/integration/user-isolation.test.ts
+++ test/integration/user-isolation.test.ts
@@ -89,10 +89,6 @@ describe("User Data Isolation Integration Tests", () => {
         { roll_number: "user1-roll1", user_id: user1Id },
         { roll_number: "user1-roll2", user_id: user1Id },
       ]);
-      const request = new Request("http://localhost/api/rolls/", {
-        method: "GET",
-      }) as unknown as NextRequest;
-
       const request = new Request("http://localhost/api/rolls") as unknown as NextRequest;
       const response = await GET(request);
       const data = await response.json();
